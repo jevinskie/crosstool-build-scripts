4c543149d34145b1bd0a262205e0091b9052058d
Jevin Sweval <jevinsweval@gmail.com>
2021-05-31 18:41:49 -0700

    builds latest rpi kernel with newlib binutils gdb and gcc from git

--- build-aarch64-toolchain.sh
+++ build-aarch64-toolchain.sh
@@ -6,32 +6,39 @@ set -o pipefail
 
 set -o xtrace
 
-NUMJOBS=16
+NUMJOBS=8
 
 JEV_GMP=gmp-6.2.1
 JEV_MPFR=mpfr-4.1.0
 JEV_MPC=mpc-1.2.1
 # JEV_GCC=gcc-10.2.0
 JEV_GCC=gcc-git
-JEV_NEWLIB=newlib-4.1.0
-JEV_BINUTILS=binutils-2.36.1
-JEV_GDB=gdb-10.2
+# JEV_NEWLIB=newlib-4.1.0
+JEV_NEWLIB=newlib-git
+# JEV_BINUTILS=binutils-2.36.1
+JEV_BINUTILS=binutils-git
+# JEV_GDB=gdb-10.2
 JEV_ISL=isl-0.24
 
-JEV_XTOOL_PREFIX=/opt/aarch64/aarch64-elf-gcc-12-git-lto
+JEV_XTOOL_PREFIX=/opt/aarch64/aarch64-elf-gcc-12-git-lto-2021-05-31
+unset GREP_OPTIONS
 
 BROOT=`brew --prefix`
 
 # brew install zlib libusb libusb-compat zstd xz libftdi gettext boost source-highlight libedit expat ncurses
+# brew install coreutils gsed make findutils gnu-tar gnu-which
+
+# export PATH=${BROOT}/opt/coreutils/libexec/gnubin:${BROOT}/opt/gsed/libexec/gnubin:${BROOT}/opt/make/libexec/gnubin:${BROOT}/opt/findutils/libexec/gnubin:${BROOT}/opt/gnu-tar/libexec/gnubin:${BROOT}/opt/gnu-which/libexec/gnubin:${PATH}
 
 export PATH=${JEV_XTOOL_PREFIX}/bin:${PATH}
 export PKG_CONFIG_PATH=${JEV_XTOOL_PREFIX}/lib/pkgconfig:${PKG_CONFIG_PATH:-}:${BROOT}/opt/zlib/lib/pkgconfig:${BROOT}/opt/libusb/lib/pkgconfig:${BROOT}/opt/libusb-compat/lib/pkgconfig:${BROOT}/opt/zstd/lib/pkgconfig:${BROOT}/opt/xz/lib/pkgconfig:${BROOT}/opt/libftdi/lib/pkgconfig:${BROOT}/opt/gettext/lib/pkgconfig:${BROOT}/opt/boost/lib/pkgconfig:${BROOT}/opt/source-highlight/lib/pkgconfig:${BROOT}/opt/libedit/lib/pkgconfig:${BROOT}/opt/expat/lib/pkgconfig:${BROOT}/opt/ncurses/lib/pkgconfig:${BROOT}/lib/pkgconfig
-export LDFLAGS=-L${JEV_XTOOL_PREFIX}/lib
+# export LDFLAGS=-L${JEV_XTOOL_PREFIX}/lib
+export LDFLAGS="-L${JEV_XTOOL_PREFIX}/lib -Wl,-rpath,${JEV_XTOOL_PREFIX}/lib"
 export CPPFLAGS=-I${JEV_XTOOL_PREFIX}/include
-export CFLAGS=${CPPFLAGS}
-export CXXFLAGS=${CPPFLAGS}
+export CFLAGS="${CPPFLAGS} -Wno-error"
+export CXXFLAGS="${CPPFLAGS} -Wno-error"
 export LDFLAGS_FOR_TARGET="-flto -fuse-linker-plugin -ffat-lto-objects"
-export CFLAGS_FOR_TARGET="${LDFLAGS_FOR_TARGET} -DPREFER_SIZE_OVER_SPEED=1 -Os -g -ffunction-sections -fdata-sections"
+export CFLAGS_FOR_TARGET="${LDFLAGS_FOR_TARGET} -DPREFER_SIZE_OVER_SPEED=1 -Os -ggdb3 -ffunction-sections -fdata-sections"
 export LIBCFLAGS_FOR_TARGET=${CFLAGS_FOR_TARGET}
 export CXXFLAGS_FOR_TARGET=${CFLAGS_FOR_TARGET}
 export LIBCXXFLAGS_FOR_TARGET=${CXXFLAGS_FOR_TARGET}
@@ -52,7 +59,7 @@ popd
 
 # mpfr
 wget -N ${JEV_GNU_MIRROR}/gnu/mpfr/${JEV_MPFR}.tar.xz
-rm -rf ${JEV_MPFR} build-mfr
+rm -rf ${JEV_MPFR} build-mpfr
 tar xf ${JEV_MPFR}.tar.xz
 mkdir -p build-mpfr
 pushd build-mpfr
@@ -81,9 +88,10 @@ make -j${NUMJOBS} install
 popd
 
 # binutils
-wget -N ${JEV_GNU_MIRROR}/gnu/binutils/${JEV_BINUTILS}.tar.bz2
-rm -rf ${JEV_BINUTILS} build-binutils
-tar xf ${JEV_BINUTILS}.tar.bz2
+# wget -N ${JEV_GNU_MIRROR}/gnu/binutils/${JEV_BINUTILS}.tar.bz2
+# rm -rf ${JEV_BINUTILS}
+rm -rf build-binutils
+# tar xf ${JEV_BINUTILS}.tar.bz2
 mkdir -p build-binutils
 pushd build-binutils
 ../${JEV_BINUTILS}/configure --prefix=${JEV_XTOOL_PREFIX} --enable-languages=c,c++ --disable-nls --enable-multiarch --enable-sysroot --enable-plugin --target=aarch64-none-elf
@@ -92,19 +100,16 @@ make -j${NUMJOBS} install
 popd
 
 # newlib unpack
-wget -N http://sourceware.org/pub/newlib/${JEV_NEWLIB}.tar.gz
-rm -rf ${JEV_NEWLIB} build-newlib
-tar xf ${JEV_NEWLIB}.tar.gz
+# wget -N http://sourceware.org/pub/newlib/${JEV_NEWLIB}.tar.gz
+# rm -rf ${JEV_NEWLIB}
+rm -rf build-newlib
+# tar xf ${JEV_NEWLIB}.tar.gz
 
 # gcc
 # wget -N ${JEV_GNU_MIRROR}/gnu/gcc/${JEV_GCC}/${JEV_GCC}.tar.xz
 # rm -rf ${JEV_GCC} build-gcc
 # tar xf ${JEV_GCC}.tar.xz
 rm -rf build-gcc
-
-# pushd ${JEV_GCC}
-# patch -p 0 < ../gcc-10-aarch64-cxx11-build-fix.patch
-# popd
 
 mkdir -p build-gcc
 pushd build-gcc
@@ -129,14 +134,17 @@ make install
 popd
 
 # gdb
-wget -N ${JEV_GNU_MIRROR}/gnu/gdb/${JEV_GDB}.tar.xz
-rm -rf ${JEV_GDB} build-gdb
-tar xf ${JEV_GDB}.tar.xz
+# handled by binutils-git
 
-mkdir -p build-gdb
-pushd build-gdb
-../${JEV_GDB}/configure --prefix=${JEV_XTOOL_PREFIX} --enable-languages=c,c++ --enable-multiarch --enable-multilib --enable-sysroot --enable-plugin --target=aarch64-none-elf
-make -j${NUMJOBS} all
-make -j${NUMJOBS} install
-popd
+# # wget -N ${JEV_GNU_MIRROR}/gnu/gdb/${JEV_GDB}.tar.xz
+# # rm -rf ${JEV_GDB}
+# rm -rf build-gdb
+# # tar xf ${JEV_GDB}.tar.xz
+
+# mkdir -p build-gdb
+# pushd build-gdb
+# ../${JEV_GDB}/configure --prefix=${JEV_XTOOL_PREFIX} --enable-languages=c,c++ --enable-multiarch --enable-multilib --enable-sysroot --enable-plugin --target=aarch64-none-elf
+# make -j${NUMJOBS} all
+# make -j${NUMJOBS} install
+# popd
 
