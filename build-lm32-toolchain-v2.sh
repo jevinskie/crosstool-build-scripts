dfacf1f065fed7557c697da6f0ac576f4171f68b
Jevin Sweval <jevinsweval@gmail.com>
2024-05-25 11:48:26 -0400

    add more deps

--- build-lm32-toolchain-v2.sh
+++ build-lm32-toolchain-v2.sh
@@ -44,7 +44,7 @@ else
     case "${UNAME_S}" in
         Darwin)
             if type brew &>/dev/null; then
-                brew install make pkg-config gnu-tar openssl readline sqlite3 xz zstd zlib bzip2 texinfo
+                brew install autoconf automake libtool make pkg-config gnu-tar openssl readline sqlite3 xz zstd zlib bzip2 texinfo tcl-tk
                 JEV_BREW_ROOT=$(brew --prefix)
                 export PATH="${JEV_BREW_ROOT}/bin:${PATH}"
                 export PKG_CONFIG_PATH="${JEV_BREW_ROOT}/lib/pkgconfig:${PKG_CONFIG_PATH}"
@@ -59,7 +59,7 @@ else
         Linux)
             if type apt &>/dev/null; then
                 sudo apt update
-                sudo apt install -y make pkg-config tar openssl libssl-dev libreadline-dev libsqlite3-dev xz-utils liblzma-dev zstd libzstd-dev zlib1g-dev bzip2 libbz2-dev texinfo
+                sudo apt install -y build-essential autoconf automake libtool make pkg-config tar openssl libssl-dev libreadline-dev libsqlite3-dev xz-utils liblzma-dev zstd libzstd-dev zlib1g-dev bzip2 libbz2-dev texinfo tcl tk tcl-dev tk-dev
             else
                 echo "Linux without apt is not supported." >&2
                 exit 1
